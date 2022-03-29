bf065ca81c710ef481c6b7d740cb277684e401ec
s-khandakar <34068482+s-khandakar@users.noreply.github.com>
2022-03-29 12:24:24 +0200

    Add files via upload

--- sumFromArray.js
+++ sumFromArray.js
@@ -1,8 +1,3 @@
-// Question 2: Write a javascript function that takes an array 
-//of numbers and a target number. The function should find two different 
-//numbers in the array that, when added together, give the target number. 
-// For example: answer([1,2,3], 4)should return [1,3]
-
 const array = [1,2,3];
 
 const sumFromArray = (array, target) => {
